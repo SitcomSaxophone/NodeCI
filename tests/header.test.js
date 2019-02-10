9e0411f0e1cd9f04615a824eb234b5c9ae28d905
Samuel Solberg <pauliesays65@gmail.com>
2019-02-10 17:20:51 -0600

    added blogs test file

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,24 +1,20 @@
-const puppeteer = require('puppeteer');
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
+const Page = require('./helpers/page');
 
-let browser, page;
+let page;
 
-beforeEach(async () => { // create new Chromium broswer/page instance before each test
-    browser = await puppeteer.launch({
-        headless: false,
-    });
-    page = await browser.newPage();
+beforeEach(async () => {
+    page = await Page.build();
+
     await page.goto('localhost:3000'); // direct browser instance to 'localhost:3000'
 }); // END create browser/page instance
 
 afterEach(async () => {
-    await browser.close(); // close Chromium instance after each test
+    await page.close(); // close Chromium instance after each test
 }); // END close Chromium instance
 
 test('The header has the correct text', async () => {
 
-    const text = await page.$eval('a.brand-logo', el => el.innerHTML); // read text of HTML element
+    const text = await page.getContentsOf('a.brand-logo'); // read text of HTML element
 
     expect(text).toEqual('Blogster'); // confirm element text reads 'Blogster'
 }); // END Header text test
@@ -32,15 +28,9 @@ test('Clicking login asserts Oauth flow', async () => {
 }); // END Login click test
 
 test('When signed in, shows Logout button', async () => {
-    const user = await userFactory();
-    const { session, sig } = sessionFactory(user);
+    await page.login();
 
-    await page.setCookie({ name: 'session', value: session }); // set user cookie-session
-    await page.setCookie({ name: 'session.sig', value: sig }); // set user session signature
-    await page.goto('localhost:3000');
-    await page.waitFor('a[href="/auth/logout"]'); // wait for element to render
-
-    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML); // read text of HTML element
+    const text = await page.getContentsOf('a[href="/auth/logout"]'); // read text of HTML element
 
     expect(text).toEqual('Logout'); // confirm element text reads 'Logout' 
 }); // END Logout button text test
