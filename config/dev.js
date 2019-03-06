81ff27f564015d91c540afd2a3bb76830f197985
Samuel Solberg <pauliesays65@gmail.com>
2019-03-06 16:27:18 -0600

    untracked dev.js

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://SatanicPanic:NukemHigh666@ds143683.mlab.com:43683/blogster_dev',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379',
-  
-};
