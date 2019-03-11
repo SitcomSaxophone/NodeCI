6948665adb830b881ad88f24740b93c6016bca6b
Samuel Solberg <pauliesays65@gmail.com>
2019-03-11 17:31:13 -0500

    finished course | edited S3 to include region and signature version to avoid 400 errors | render uploaded images

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -7,6 +7,15 @@ class BlogShow extends Component {
     this.props.fetchBlog(this.props.match.params._id);
   }
 
+  renderImage() {
+    if (this.props.blog.imageUrl) {
+      return (<img
+        src={'https://s3.us-east-2.amazonaws.com/my-node-blog/' + this.props.blog.imageUrl}
+        alt=""
+      />)
+    }
+  }
+
   render() {
     if (!this.props.blog) {
       return '';
@@ -18,6 +27,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
